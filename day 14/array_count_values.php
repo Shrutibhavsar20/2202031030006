08ce9b793825e049da5376fede13091cb97644f4
Shrutibhavsar20 <137257153+Shrutibhavsar20@users.noreply.github.com>
2023-07-07 10:55:04 +0530

    Add files via upload

---
+++ day 14/array_count_values.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>array count values function</title>
+</head>
+<body>
+    <?php
+    $a=array("gunjan","shruti","gunjan","gunjan","hshruti","shruti","gunjan");
+    print_r(array_count_values($a));
+    ?>
+</body>
+</html>
